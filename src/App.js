eac757814a780df974322123542ca4936f723ffc
raju.kapadne09@gmail.com <Shimpi@1989>
2021-05-29 19:55:49 +0530

    Dashboad message changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Pradnyesh <code>Kapadne</code> from pune.
         </p>
         <a
           className="App-link"
